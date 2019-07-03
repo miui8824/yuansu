e5fb987700d8eca8eed6b69e35bc3d0cc9687ebc
dreams88 <781306919@qq.com>
2019-07-03 23:55:06 +0800

    修改样式

--- pages/mySelf/mySelf.js
+++ pages/mySelf/mySelf.js
@@ -8,46 +8,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showIcon: false,//引入的自定义导航左边返回按钮
-    title:'我的',
+    showIcon: false, //引入的自定义导航左边返回按钮
+    title: '我的',
     // 我是一个小逗比
-    painting: {},  //这个是 用canvas画图用的 变量
-    shareImage: '',  //这个是 用canvas画图用的 变量
-    GetImage:false,
+    painting: {}, //这个是 用canvas画图用的 变量
+    shareImage: '', //这个是 用canvas画图用的 变量
+    GetImage: false,
     icon_down: 'https://file.maiyatown.com/images/mySelf/kuozhan.svg',
-    my_Inviter:false,
-    tabbar:{},
+    my_Inviter: false,
+    tabbar: {},
     fansCount: "",
     todayFans: "",
     yesterdayFans: "",
-    avlBal:"",//账户余额
-    couponSum:"",//优惠券
-    frtCommission:"",//待遇余额
-    headImageUrl:"",//用户头像
-    integral:"积分",//积分
-    isBdPhone: "1",//是否绑定手机
-    isBdWX:"1",//是否绑定微信
-    isBdInviter:'',//是否绑定邀请码
-    nickName: "",//用户昵称
-    userLevel:"",//是否为普通用户
-    integral:"",//积分
-    funsCount:"",//总的粉丝人
-    inviterHeadImg:"",//邀请人头像
-    inviterNickName:"",//邀请人昵称像
-    inviterTime: "",//邀请时间
-    todayFuns:"",
-    yesterdayFuns:"",
-    wechat_num:"",
-    inviterOk:false,//邀请人详情
-    hasInviter:false,
+    avlBal: "", //账户余额
+    couponSum: "", //优惠券
+    frtCommission: "", //待遇余额
+    headImageUrl: "", //用户头像
+    integral: "积分", //积分
+    isBdPhone: "1", //是否绑定手机
+    isBdWX: "1", //是否绑定微信
+    isBdInviter: '', //是否绑定邀请码
+    nickName: "", //用户昵称
+    userLevel: "", //是否为普通用户
+    integral: "", //积分
+    funsCount: "", //总的粉丝人
+    inviterHeadImg: "", //邀请人头像
+    inviterNickName: "", //邀请人昵称像
+    inviterTime: "", //邀请时间
+    todayFuns: "",
+    yesterdayFuns: "",
+    wechat_num: "",
+    inviterOk: false, //邀请人详情
+    hasInviter: false,
 
-    userId:null,
-    daifk_num:'',
-    daish_num:'',
+    userId: null,
+    daifk_num: '',
+    daish_num: '',
 
-    groupTimeData:[],
-    appointmentTimeData:[],
-    userid_weweima:""
+    groupTimeData: [],
+    appointmentTimeData: [],
+    userid_weweima: "",
+    tabmeun: [{
+        path: '/pages/myRegiment/myRegiment',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703233324747.png',
+        text: '我的拼团'
+      },
+      {
+        path: '',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703233429828.png',
+        text: '积分商城'
+      },
+      {
+        path: '/pages/addressManagement/addressManagement',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703233503603.png',
+        text: '收货地址'
+      },
+      {
+        path: '',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703234219789.png',
+        text: '实名认证'
+      },
+      {
+        path: '',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703234254563.png',
+        text: '帮助客服'
+      },
+      {
+        path: '',
+        url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190703234340419.png',
+        text: '设置'
+      }
+    ]
   },
   // canvas画图分享的发放 （保存图片，绘制分享图）
   eventDraw() {
@@ -90,105 +121,105 @@ Page({
           this.setData({
             userid_weweima: data.data
           })
-       
-      
-        //回到顶部
-        wx.pageScrollTo({
-          scrollTop: 0
-        })
-        wx.showLoading({
-          title: '绘制分享图片中',
-          mask: true
-        })
-        console.log(this.data.copywriting, this.data.imgUrl)
-        this.setData({
-          painting: {
-            width: 375,
-            height: 555,
-            clear: true,
-            views: [
-              // 背景白图
-              {
-                type: 'image',
-                url: 'https://file.maiyatown.com/images/v1.2/home_bg.png',
-                top: 0,
-                left: 0,
-                width: 375,
-                height: 555
-              },
-              // 最上面的小麦
-              {
-                type: 'image',
 
-                url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190621165146153.png',
-                top: 12,
-                left: 99,
-                width: 35,
-                height: 35
-              },
 
-              {
-                type: 'text',
-                content: '元素城堡',
-                fontSize: 16,
-                color: '#402D16',
-                textAlign: 'left',
-                top: 20,
-                left: 146,
-                bolder: true
-              },
-              // 中间的图 this.data.imgUrl
-              {
-                type: 'image',
-                url: this.data.imgUrl ,
-                top: 60,
-                left: 32.5,
-                width: 310,
-                height: 350
-              },
-              // 二维码图片
-              {
-                type: 'image',
-                url: this.data.userid_weweima,
-                top: 420,
-                left: 250,
-                width: 88,
-                height: 88
-              },
-              // 动态生成的字
-              {
-                type: 'text',
-                content: this.data.copywriting,
-                fontSize: 14,
-                color: '#101010',
-                textAlign: 'left',
-                top: 440,
-                left: 32.5,
-                lineHeight: 20,
-                MaxLineNumber: 2,
-                breakWord: true,
-                width: 155,
-                bolder: true
-              },
-              // 二维码下面的字
-              {
-                type: 'text',
-                content: "长按识别小程序码",
-                fontSize: 12,
-                color: '#999999',
-                textAlign: 'left',
-                top: 520,
-                left: 240,
-                lineHeight: 20,
-                // MaxLineNumber: 2,
-                // breakWord: true,
-                width: 155,
-                bolder: true
-              }
-            ]
-          }
+          //回到顶部
+          wx.pageScrollTo({
+            scrollTop: 0
+          })
+          wx.showLoading({
+            title: '绘制分享图片中',
+            mask: true
+          })
+          console.log(this.data.copywriting, this.data.imgUrl)
+          this.setData({
+            painting: {
+              width: 375,
+              height: 555,
+              clear: true,
+              views: [
+                // 背景白图
+                {
+                  type: 'image',
+                  url: 'https://file.maiyatown.com/images/v1.2/home_bg.png',
+                  top: 0,
+                  left: 0,
+                  width: 375,
+                  height: 555
+                },
+                // 最上面的小麦
+                {
+                  type: 'image',
+
+                  url: 'http://yscb.oss-cn-shanghai.aliyuncs.com/pro/static/img/20190621165146153.png',
+                  top: 12,
+                  left: 99,
+                  width: 35,
+                  height: 35
+                },
+
+                {
+                  type: 'text',
+                  content: '元素城堡',
+                  fontSize: 16,
+                  color: '#402D16',
+                  textAlign: 'left',
+                  top: 20,
+                  left: 146,
+                  bolder: true
+                },
+                // 中间的图 this.data.imgUrl
+                {
+                  type: 'image',
+                  url: this.data.imgUrl,
+                  top: 60,
+                  left: 32.5,
+                  width: 310,
+                  height: 350
+                },
+                // 二维码图片
+                {
+                  type: 'image',
+                  url: this.data.userid_weweima,
+                  top: 420,
+                  left: 250,
+                  width: 88,
+                  height: 88
+                },
+                // 动态生成的字
+                {
+                  type: 'text',
+                  content: this.data.copywriting,
+                  fontSize: 14,
+                  color: '#101010',
+                  textAlign: 'left',
+                  top: 440,
+                  left: 32.5,
+                  lineHeight: 20,
+                  MaxLineNumber: 2,
+                  breakWord: true,
+                  width: 155,
+                  bolder: true
+                },
+                // 二维码下面的字
+                {
+                  type: 'text',
+                  content: "长按识别小程序码",
+                  fontSize: 12,
+                  color: '#999999',
+                  textAlign: 'left',
+                  top: 520,
+                  left: 240,
+                  lineHeight: 20,
+                  // MaxLineNumber: 2,
+                  // breakWord: true,
+                  width: 155,
+                  bolder: true
+                }
+              ]
+            }
+          })
         })
-      })
       }
     })
 
@@ -212,19 +243,28 @@ Page({
           duration: 2000
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res)
         if (res.errMsg === "saveImageToPhotosAlbum:fail:auth denied" || res.errMsg === "saveImageToPhotosAlbum:fail auth deny" || res.errMsg === "saveImageToPhotosAlbum:fail authorize no response") {
           console.log("打开设置窗口");
-        that.imageRrrorAuth();
+          that.imageRrrorAuth();
         }
       }
     })
   },
-  share_hide: function () {
+  share_hide: function() {
     this.setData({
       GetImage: false
     })
+  },
+  navto(e) {
+
+    if (e.currentTarget.dataset.item.path !== '') {
+      wx.navigateTo({
+        url: e.currentTarget.dataset.item.path,
+      })
+    }
+
   },
   // 微信改版弹框授权
   imageRrrorAuth() {
@@ -264,26 +304,29 @@ Page({
   eventGetImage(event) {
     console.log(event)
     wx.hideLoading()
-    const { tempFilePath, errMsg } = event.detail
+    const {
+      tempFilePath,
+      errMsg
+    } = event.detail
     if (errMsg === 'canvasdrawer:ok') {
       this.setData({
         shareImage: tempFilePath
       })
     }
   },
-  onLoad(){
+  onLoad() {
 
   },
-  onReady(){
-    
+  onReady() {
+
     // this.getFans();
     this.maself_info();
     this.getCouponsList();
     this.myOrderCountInfo();
   },
-  onShow: function (options) {
+  onShow: function(options) {
     wx.setNavigationBarTitle({
-      title: "我的"//页面标题为路由参数  
+      title: "我的" //页面标题为路由参数  
     })
     if (wx.getStorageSync('myxzToken')) {
       this.maself_info();
@@ -300,13 +343,13 @@ Page({
     this.myOrderCountInfo();
     app.finduser_info()
   },
-  onHide: function () {
+  onHide: function() {
     this.setData({
       GetImage: false,
     })
   },
   // 获取我的订单待付款和待收货订单数量
-  myOrderCountInfo(){
+  myOrderCountInfo() {
     let token = wx.getStorageSync('myxzToken');
     let reqObj = {
       url: '/api/my/myOrderCountInfo? auth-token=' + token,
@@ -314,7 +357,11 @@ Page({
     }
     util.RequestGet(reqObj, null, (res, message) => {
       if (message) {
-        wx.showToast({ title: res.resDesc, icon: 'none', duration: 1500 });
+        wx.showToast({
+          title: res.resDesc,
+          icon: 'none',
+          duration: 1500
+        });
       }
       if (res.resCode == '0000') {
         // wx.showToast({ title: res.resDesc, icon: 'none', duration: 1500 });
@@ -327,32 +374,15 @@ Page({
     })
   },
 
-  show_Inviter(){
-    // const that = this;
-    // if (this.data.my_Inviter ==false){
-    //   this.setData({
-    //     icon_down: '/images/mySelf/down.svg',
-    //     my_Inviter: true
-    //   })
-    // }else{
-    //   this.setData({
-    //     icon_down: '/images/mySelf/kuozhan.svg',
-    //     my_Inviter: false
-    //   })
-    // }
-    wx.navigateTo({
-      url: '/pages/addressManagement/addressManagement',
-    })
-  },
   //埋点点击客服
   mdClick() {
     util.mdFun(this, '点击客服', '');
   },
-  maself_info(){
+  maself_info() {
     let that = this
     let token = wx.getStorageSync('myxzToken');
-    let url = apiUrl.API_URL +'/api/my/acct';
-  
+    let url = apiUrl.API_URL + '/api/my/acct';
+
     if (token) {
       // let header = Object.assign({ 'auth-token': token }, headerData);
       wx.request({
@@ -381,12 +411,12 @@ Page({
             integral: res.data.data.integral,
             // couponSum: res.data.data.couponSum,
             avlBal: Number(res.data.data.avlBal).toFixed(2),
-            wechat_num:res.data.data.wxNum,
+            wechat_num: res.data.data.wxNum,
           });
         }
       })
     } else {
-     
+
       wx.navigateTo({
         url: "/pages/init/init"
       })
@@ -397,7 +427,7 @@ Page({
     let reqObj = {
       url: '/api/user/getPrizeGrant',
       data: {
-        couponType:  1
+        couponType: 1
       }
     }
     util.RequestGet(reqObj, null, (res, message) => {
@@ -410,25 +440,25 @@ Page({
       }
       if (res.resCode == '0000') {
         //设置标题
-     
+
         this.setData({
-          couponSum:res.resultCount
+          couponSum: res.resultCount
         })
-        
+
       }
     })
   },
   //点击我的邀请人详情
-  inviterDetails(){
+  inviterDetails() {
     // if (!inviterOk) {
     //   return false
     // }
-    let inviterOk = this.data.inviterOk?false:true;  
+    let inviterOk = this.data.inviterOk ? false : true;
     this.setData({
       inviterOk
     })
   },
-  bindwechat(option){
+  bindwechat(option) {
     console.log(option)
     let isbdinviter = option.currentTarget.dataset.isbdinviter
     let bdInviterCode = option.currentTarget.dataset.bdinvitercode
@@ -437,9 +467,9 @@ Page({
       url: '/pages/bindInviteCode/bindInviteCode?isbdinviter=' + isbdinviter + '&bdInviterCode=' + bdInviterCode,
     })
   },
-  bindphone(e){
-   
-    if (this.data.isBdPhone == 1){
+  bindphone(e) {
+
+    if (this.data.isBdPhone == 1) {
       // Toast.fail('不能修改已绑手机号');
       // console.log('daa')
       // wx.showToast({
@@ -448,63 +478,63 @@ Page({
       //   duration: 1000,
       //   mask: true
       // })
-    }else{
+    } else {
       wx.navigateTo({
         url: '/pages/bindPhone/bindPhone',
       })
     }
   },
-  Cash_withdrawal(){
+  Cash_withdrawal() {
     // Toast('提现功能即将上线，请耐心期待！');
-    
-    if (this.data.isBdPhone==1){
+
+    if (this.data.isBdPhone == 1) {
       wx.navigateTo({
         url: '/pages/extractCash/extractCash?user_leval=' + this.data.userLevel,
       })
-    }else{
+    } else {
       Toast('请输入正确的手机号');
     }
 
   },
-  Detailed(){
+  Detailed() {
     wx.navigateTo({
       url: '/pages/balanceDetail/balanceDetail',
     })
   },
   //去卷包
-  coupons(){
+  coupons() {
     wx.navigateTo({
       url: '/pages/coupons/coupons'
     })
   },
   // 去积分
-  integral(){
+  integral() {
     wx.navigateTo({
       url: '/pages/myIntegral/myIntegral'
     })
   },
   //我的粉丝
-  myfans(){
+  myfans() {
     wx.navigateTo({
       url: '/pages/myFans/myFans?fansNum=' + this.data.funsCount
     })
   },
   //去全部订单
-  goToMyOrder(){
+  goToMyOrder() {
     wx.navigateTo({
-      url:'/pages/myOrder/myOrder'
+      url: '/pages/myOrder/myOrder'
     })
   },
-  orderJump(event){
+  orderJump(event) {
     wx.navigateTo({
       url: '/pages/myOrder/myOrder?navActive=' + event.currentTarget.dataset.index
     })
   },
-  order_sale(event){
+  order_sale(event) {
     wx.navigateTo({
       url: '/pages/afterSale_list/afterSale_list?navActive=' + event.currentTarget.dataset.index
     })
-    
+
   },
   //获取粉丝
   // getFans(){
@@ -525,16 +555,16 @@ Page({
   //     }
   //   })
   // }
- 
-  onShareAppMessage: function (res) {
+
+  onShareAppMessage: function(res) {
     // this.setData({
     //   GetImage: false
     // })
     let shareImageArr = wx.getStorageSync('shareImage');
-    let shareImage='';
+    let shareImage = '';
     let numberText = Math.floor(Math.random() * 4);
     //let userId = wx.getStorageSync('myxzUserId');
-    if (shareImageArr&&shareImageArr.length>0){
+    if (shareImageArr && shareImageArr.length > 0) {
       shareImage = shareImageArr[Math.floor(Math.random() * shareImageArr.length)].bannerUrl;
     }
     let reqObj = {
@@ -556,20 +586,14 @@ Page({
     }
   },
 
-  onPullDownRefresh(){
+  onPullDownRefresh() {
     this.maself_info();
     this.getCouponsList();
     this.myOrderCountInfo();
     wx.stopPullDownRefresh()
   },
-  //点击进入我的拼团
-  my_Regiment(){
-    wx.navigateTo({
-      url: '/pages/myRegiment/myRegiment' 
-    })
-  },
   //待结算余额明细
-  preBalance(){
+  preBalance() {
     wx.navigateTo({
       url: "/pages/preBalanceDetail/preBalanceDetail"
     })
